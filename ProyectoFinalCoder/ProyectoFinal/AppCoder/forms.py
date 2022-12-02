24483e86eb1d497d13ffcef4584501aa1e397093
Jeremías Hernández <70614242+hjeremias@users.noreply.github.com>
2022-12-01 23:45:20 -0300

    Delete ProyectoFinalCoder directory

--- ProyectoFinalCoder/ProyectoFinal/AppCoder/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
- 
-class JugadorForm(forms.Form):
-    nombre = forms.CharField()
-    equipo = forms.CharField()
-
-class EquipoForm(forms.Form):
-    nombre = forms.CharField()
-    dt = forms.CharField()
